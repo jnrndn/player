493947ca4e79dfbd88f81554e99e2e35c52f87a6
Juan Rendon <juan.rendon@yuxiglobal.com>
2017-08-18 16:50:35 -0500

    transitions between slides fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { VgAPI } from 'videogular2/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/merge';
 
 export interface ISource {
   url: string;
@@ -18,52 +19,56 @@ export class AppComponent {
 
   firstVideo: IPlayable;
   secondVideo: IPlayable;
+  thirdVideo: IPlayable;
   audio: IPlayable;
   api: VgAPI;
+  flag = true;
 
   source: Array<ISource> = [
     // tslint:disable-next-line:max-line-length
-    { url: 'https://apexsct-my.sharepoint.com/personal/carlos_angulo_yuxiglobal_com/Documents/Microsoft%20Teams%20Chat%20Files/matrix-min.mp4' },
-    { url: 'https://apexsct-my.sharepoint.com/personal/carlos_angulo_yuxiglobal_com/Documents/Microsoft%20Teams%20Chat%20Files/inmortals-min.mp4' },
+    { url: 'https://apexsct-my.sharepoint.com/personal/juan_rendon_yuxiglobal_com/Documents/Microsoft%20Teams%20Chat%20Files/matrix-min.mp4' },
+    { url: 'https://apexsct-my.sharepoint.com/personal/juan_rendon_yuxiglobal_com/Documents/Microsoft%20Teams%20Chat%20Files/inmortals-min.mp4' },
     // tslint:disable-next-line:max-line-length
-    { url: 'https://apexsct-my.sharepoint.com/personal/carlos_angulo_yuxiglobal_com/Documents/Microsoft%20Teams%20Chat%20Files/league-mn.mp4' },
+    { url: 'https://apexsct-my.sharepoint.com/personal/juan_rendon_yuxiglobal_com/Documents/Microsoft%20Teams%20Chat%20Files/league-mn.mp4' },
   ];
 
   currentIndex = 0;
   currentItem: ISource = this.source[this.currentIndex];
+  secondItem: ISource = this.source[this.currentIndex + 1];
 
   constructor() { }
 
-  /**
-   * How is the video made? does the user have to check by himself the length of
-   * the narration video and all other videos to ensure the other videos match the
-   * length of the narration video?
-   *
-   * How do we get the information of the API, can we ensure that we get exactly
-   * the amount of videos with the duration such as they match the narration one?
-   */
   onPlayerReady(api: VgAPI) {
     this.api = api;
     this.firstVideo =  this.api.getMediaById('firstVideo');
     this.secondVideo = this.api.getMediaById('secondVideo');
+    this.thirdVideo = this.api.getMediaById('thirdVideo');
     this.audio = this.api.getMediaById('backgroundAudio');
-    this.secondVideo.subscriptions.loadedMetadata.subscribe(() => {
+    console.log(this.secondVideo);
+    this.secondVideo.subscriptions.loadedMetadata.merge(
+      this.thirdVideo.subscriptions.loadedMetadata
+    ).subscribe(() => {
+      let currentTime = 0;
       if (this.currentIndex === 0) {
-        this.firstVideo.currentTime = 0;
-        this.audio.currentTime = 0;
+        currentTime = 0;
       } else if (this.currentIndex === 1) {
-        this.firstVideo.currentTime = 20;
-        this.audio.currentTime = 20;
+        currentTime = 20;
       } else {
-        this.firstVideo.currentTime = 40;
-        this.audio.currentTime = 40;
+        currentTime = 40;
       }
+      this.firstVideo.currentTime = currentTime;
+      this.audio.currentTime = currentTime;
       this.playVideo();
     });
-    this.secondVideo.subscriptions.ended.subscribe(() => {
+
+    this.secondVideo.subscriptions.ended.merge(
+      this.thirdVideo.subscriptions.ended
+    ).subscribe(() => {
+
       this.nextVideo();
       this.api.pause();
     });
+    // Todo: Set seek for slider scrub-bar
     this.secondVideo.subscriptions.seeked.subscribe(timeSeeked => {
       if (this.currentIndex === 0) {
         this.firstVideo.currentTime = timeSeeked.target.currentTime;
@@ -79,13 +84,19 @@ export class AppComponent {
   }
 
   nextVideo() {
-    this.api.pause();
+    if (this.currentIndex === this.source.length - 1) {
+      return;
+    }
+    // this.api.pause();
     this.currentIndex = ++this.currentIndex % this.source.length;
     this.currentItem = this.source[this.currentIndex];
+    this.flag = !this.flag;
   }
 
   playVideo() {
-    this.api.play();
+    this.audio.play();
+    this.firstVideo.play();
+    this.flag ? this.secondVideo.play() : this.thirdVideo.play();
   }
 
   findIndex(number) {
